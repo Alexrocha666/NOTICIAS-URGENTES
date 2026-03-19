d5319e29661094fb497b23eea7452499a9a7caf9
Alexrocha666 <alexpinposo@gmail.com>
2026-03-19 10:35:13 -0300

    Update and rename firebase.js to admin.js

---
+++ admin.js
@@ -0,0 +1,28 @@
+```javascript
+const SENHA = "1234";
+
+function login() {
+  let senha = document.getElementById("senha").value;
+
+  if (senha === SENHA) {
+    document.getElementById("login").classList.add("hidden");
+    document.getElementById("painel").classList.remove("hidden");
+  } else {
+    alert("Senha incorreta");
+  }
+}
+
+function publicar() {
+  let titulo = document.getElementById("titulo").value;
+  let conteudo = document.getElementById("conteudo").value;
+  let imagem = document.getElementById("imagem").value;
+
+  let noticias = JSON.parse(localStorage.getItem("noticias")) || [];
+
+  noticias.push({ titulo, conteudo, imagem });
+
+  localStorage.setItem("noticias", JSON.stringify(noticias));
+
+  alert("Notícia publicada!");
+}
+```
--- firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
