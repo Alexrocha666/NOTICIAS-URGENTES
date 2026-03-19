f45092e4422636e88a5f16938169ead9f40fa8bf
Alexrocha666 <alexpinposo@gmail.com>
2026-03-19 10:21:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Portal Fuxicos de Parauapebas carregado!");
+db.collection("noticias").orderBy("data", "desc").onSnapshot(snapshot => {
+  const area = document.getElementById("noticias");
+  area.innerHTML = "";
+
+  snapshot.forEach(doc => {
+    let n = doc.data();
+
+    area.innerHTML += `
+      <div class="noticia">
+        <h2>${n.titulo}</h2>
+        <img src="${n.imagem}">
+        <p>${n.conteudo}</p>
+        <small>${new Date(n.data).toLocaleString()}</small>
+      </div>
+    `;
+  });
+});
